76833976443508afbaa6787e4093e7a03d2ee51f
Denisa Alina <alinaghent8@gmail.com>
2022-06-27 23:01:31 +0200

    second commit

--- src/com/fasttrack/ExtraMile.java
+++ src/com/fasttrack/ExtraMile.java
@@ -4,19 +4,20 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ExtraMile {
-    List <Integer> maxList = new ArrayList<>();
     public int minMax(List <List < Integer>> lists ){
+        int max = (lists.get(0)).get(0);
+        int min = Integer.MAX_VALUE;
         for(int i=0; i< lists.size(); i++){
-            int max = (lists.get(i)).get(0);
             for(int j=0; j<(lists.get(i)).size(); j++){
                 max = max < (lists.get(i)).get(j) ? (lists.get(i)).get(j) : max;
+
+
             }
-            maxList.add(max);
+            min = min > max ? max : min;
+
+
         }
-        int min = maxList.get(0);
-        for(int i=0; i<maxList.size(); i++){
-            min = min > maxList.get(i) ? maxList.get(i) : min;
-        }
+
         return min;
     }
 }
